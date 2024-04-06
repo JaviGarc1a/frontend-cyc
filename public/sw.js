a2d1b7e5993549fbc49af84a8b272c9f7c67485f
JaviGarc1a <javgaragu1@alum.us.es>
2024-04-06 20:07:35 +0200

    feat: Test indexDB

--- public/sw.js
+++ public/sw.js
@@ -1,94 +1,63 @@
-/**
- * Copyright 2018 Google Inc. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+import { openDB } from 'idb'
 
-// If the loader is already loaded, just stop.
-if (!self.define) {
-  let registry = {};
+const POST_REQUESTS_STORE_NAME = 'post-requests'
 
-  // Used for `eval` and `importScripts` where we can't get script URL by other means.
-  // In both cases, it's safe to use a global var because those functions are synchronous.
-  let nextDefineUri;
-
-  const singleRequire = (uri, parentUri) => {
-    uri = new URL(uri + ".js", parentUri).href;
-    return registry[uri] || (
-      
-        new Promise(resolve => {
-          if ("document" in self) {
-            const script = document.createElement("script");
-            script.src = uri;
-            script.onload = resolve;
-            document.head.appendChild(script);
-          } else {
-            nextDefineUri = uri;
-            importScripts(uri);
-            resolve();
-          }
-        })
-      
-      .then(() => {
-        let promise = registry[uri];
-        if (!promise) {
-          throw new Error(`Module ${uri} didn’t register its module`);
-        }
-        return promise;
-      })
-    );
-  };
-
-  self.define = (depsNames, factory) => {
-    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
-    if (registry[uri]) {
-      // Module is already loading or loaded.
-      return;
-    }
-    let exports = {};
-    const require = depUri => singleRequire(depUri, uri);
-    const specialDeps = {
-      module: { uri },
-      exports,
-      require
-    };
-    registry[uri] = Promise.all(depsNames.map(
-      depName => specialDeps[depName] || require(depName)
-    )).then(deps => {
-      factory(...deps);
-      return exports;
-    });
-  };
+// Función para abrir la base de datos IndexedDB
+async function openDatabase() {
+	return await openDB('requests-db', 1, {
+		upgrade(db) {
+			db.createObjectStore(POST_REQUESTS_STORE_NAME)
+		}
+	})
 }
-define(['./workbox-7144475a'], (function (workbox) { 'use strict';
 
-  importScripts();
-  self.skipWaiting();
-  workbox.clientsClaim();
-  workbox.registerRoute("/", new workbox.NetworkFirst({
-    "cacheName": "start-url",
-    plugins: [{
-      cacheWillUpdate: async ({
-        response: e
-      }) => e && "opaqueredirect" === e.type ? new Response(e.body, {
-        status: 200,
-        statusText: "OK",
-        headers: e.headers
-      }) : e
-    }]
-  }), 'GET');
-  workbox.registerRoute(/.*/i, new workbox.NetworkOnly({
-    "cacheName": "dev",
-    plugins: []
-  }), 'GET');
-  self.__WB_DISABLE_DEV_LOGS = true;
+// Intercepta las solicitudes fetch
+self.addEventListener('fetch', event => {
+	if (event.request.method === 'POST') {
+		event.respondWith(storePostRequest(event.request))
+	} else {
+		event.respondWith(fetch(event.request))
+	}
+})
 
-}));
-//# sourceMappingURL=sw.js.map
+// Almacena la solicitud POST en IndexedDB
+async function storePostRequest(request) {
+	try {
+		const db = await openDatabase()
+		const tx = db.transaction(POST_REQUESTS_STORE_NAME, 'readwrite')
+		const store = tx.objectStore(POST_REQUESTS_STORE_NAME)
+		await store.add(request.clone())
+		return new Response('Solicitud POST almacenada localmente.')
+	} catch (error) {
+		console.error('Error al almacenar la solicitud POST:', error)
+		return new Response('Error al almacenar la solicitud POST.', {
+			status: 500
+		})
+	}
+}
+
+// Sincroniza las solicitudes almacenadas localmente con el backend
+async function syncStoredRequests() {
+	try {
+		const db = await openDatabase()
+		const tx = db.transaction(POST_REQUESTS_STORE_NAME, 'readonly')
+		const store = tx.objectStore(POST_REQUESTS_STORE_NAME)
+		const requests = await store.getAll()
+		for (const request of requests) {
+			const response = await fetch(request)
+			if (response.ok) {
+				await store.delete(request)
+			}
+		}
+		console.log('Sincronización de solicitudes POST completada.')
+	} catch (error) {
+		console.error('Error al sincronizar las solicitudes POST:', error)
+	}
+}
+
+// Escucha eventos de sincronización para manejar la sincronización de solicitudes POST almacenadas
+self.addEventListener('sync', event => {
+	if (event.tag === 'sync-post-requests') {
+		event.waitUntil(syncStoredRequests())
+	}
+})
